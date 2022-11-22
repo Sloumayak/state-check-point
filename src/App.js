ca8a046b4a3095773f3ade6066ccc51215ede0bc
slouma <yacoubiabdessalem95@gmail.com>
2022-11-22 21:24:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Home from "./Home";
+class App extends Component {
+  state = { count: 0, show: false, text:'' ,finalText:'' };
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleDecriment = () => {
+    this.state.count > 0 && this.setState({ count: this.state.count - 1 });
+  };
+  handleShow = () => {
+    this.state.show
+      ? this.setState({ show: false })
+      : this.setState({ show: true });
+  };
+  handleChange = (e) => {
+    this.setState({...this.state,text: e.target.value});
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({...this.state,finalText:this.state.text});
+  }
+  render() {
+    return (
+      <>
+        <div>
+          <button onClick={this.handleIncrement}>+</button>
+          <h1>{this.state.count}</h1>
+          <button onClick={this.handleDecriment}>-</button>
+        </div>
+        <button onClick={this.handleShow}> SHOW</button>
+        {this.state.show && <Home />}
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value={this.state.text} />
+          <button type="submit">submit</button>
+        </form>
+        <h2>{this.state.finalText}</h2>
+      </>
+    );
+  }
 }
 
 export default App;
